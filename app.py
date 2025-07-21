095fdc1770a95d4d0d649014f928b694629afea3
rindoshingen <zenjukai@gmail.com>
2025-07-21 06:44:31 -0600

    Added Tailwindcss and Alpine JS

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World!"
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    return render_template("index.html")
