a61b063c96112a33b057d96dcad7955322629294
rindoshingen <zenjukai@gmail.com>
2025-07-21 07:58:28 -0600

    Add a Jobs dictionary + dynamic template tags in index.html.

--- app.py
+++ app.py
@@ -2,6 +2,33 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+      'id': 1,
+      'title': 'Data Analyst',
+      'location': 'Victoria, BC',
+      'salary': '70K-90K'  
+    },
+    {
+      'id': 2,
+      'title': 'Frontend Developer',
+      'location': 'Calgary, AB',
+      'salary': '50K-60K'  
+    },
+    {
+      'id': 3,
+      'title': 'Backend Developer',
+      'location': 'Vancouver, BC',
+      'salary': '80K-110K'  
+    },
+    {
+      'id': 4,
+      'title': 'Security Analyst',
+      'location': 'Kelowna, BC',
+      'salary': ''  
+    }
+]
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", jobs=JOBS, company_name="Careers Website")
